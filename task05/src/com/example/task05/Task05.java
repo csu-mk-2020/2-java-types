9e8957d6ceb199504c64cfe34c45b5423e28aee2
Arthur <parabellum357@hotmail.com>
2020-09-12 14:35:12 +0500

    Task 05 [Саитгалин]

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -4,9 +4,12 @@ public class Task05 {
 
     public static String solution(int x) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        for (; x != 0; x/=10) {
+            if (x%2 == 1)
+                return "FALSE";
+        }
 
-        return "FALSE";
+        return "TRUE";
     }
 
     public static void main(String[] args) {
