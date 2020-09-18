20ac2e52c6ac737fa201603587615e71f2169998
funin <funin99@bk.ru>
2020-09-19 00:51:31 +0500

    Task03 [Фунин]

--- task03/src/com/example/task03/Task03.java
+++ task03/src/com/example/task03/Task03.java
@@ -3,10 +3,8 @@ package com.example.task03;
 public class Task03 {
 
     public static int getMetreFromCentimetre(int centimetre) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        centimetre /= 100;
+        return centimetre;
     }
 
     public static void main(String[] args) {
