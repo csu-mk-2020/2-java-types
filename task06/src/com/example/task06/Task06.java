01bd292702ce2074e184742282c2d31d0570f4ff
Kirito1899 <alex-kadykov@bk.ru>
2020-09-18 12:45:39 +0500

    Task06 [Кадыков]

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -2,20 +2,24 @@ package com.example.task06;
 
 public class Task06 {
 
+    public static int getCountsOfDigits(long number) {
+        return (number == 0) ? 1 : (int) Math.ceil(Math.log10(Math.abs(number) + 0.5));
+    }
+
     public static int solution(int x, int y) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
-        return 0;
+        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        return getCountsOfDigits(x + y);
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         int result = solution(12, 34);
         System.out.println(result);
-        */
+
     }
 
 }
