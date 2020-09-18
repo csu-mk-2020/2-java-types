3ec6e982026b94f0bb9dbd39fac65b8b3f511e8f
Jester <zavyalov.mikhail99@gmail.com>
2020-09-18 18:56:29 +0500

    task05[Zavyalov]

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -5,9 +5,15 @@ public class Task05 {
     public static String solution(int x) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        for (; x != 0; x/=10) {
+            if (x%2 == 1)
+                return "FALSE";
+        }
 
-        return "FALSE";
+
+        return "TRUE";
     }
+
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
