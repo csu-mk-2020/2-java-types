abfe00a601510aae97d4849ece7c69e2469e0168
Kirito1899 <alex-kadykov@bk.ru>
2020-09-18 12:44:40 +0500

    Task05 [Кадыков]

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -5,17 +5,24 @@ public class Task05 {
     public static String solution(int x) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        int ostatok;
+        while (x > 0) {
+            ostatok = x % 10;
+            if (ostatok % 2 != 0) {
+                return "FALSE";
+            }
+            x = x / 10;
+        }
+        return "TRUE";
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         String result = solution(1234);
         System.out.println(result);
-        */
+
     }
 
 }
