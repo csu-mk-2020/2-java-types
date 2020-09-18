53daef1427d9d1e2ffc717785f1d440fd6e7ff9b
Jester <zavyalov.mikhail99@gmail.com>
2020-09-18 19:25:15 +0500

    task10[Zavyalov]

--- task10/src/com/example/task10/Task10.java
+++ task10/src/com/example/task10/Task10.java
@@ -7,9 +7,26 @@ public class Task10 {
         // TODO корректно сравнивать два значения типа float с заданной пользователем точностью (параметр - количество знаков после запятой).
         // Функция должна корректно обрабатывать ситуацию со сравнением значений бесконечности.
         // Функция должна считать значения «не число» NaN (например 0.0/0.0) равными между собой.
-        
-        return a == b;
 
+        if (Float.isFinite(a) && Float.isFinite(b)){
+
+            String aString = Float.toString(a);
+            if(aString.indexOf(".")+precision + 1  < aString.length())
+                aString = aString.substring(0, aString.indexOf(".")+precision);
+
+            String bString = Float.toString(b);
+            if(bString.indexOf(".")+precision + 1 < bString.length())
+                bString = bString.substring(0, bString.indexOf(".")+precision);
+
+            return Float.compare(new Float(aString), new Float(bString)) == 0;
+        }
+        else if (Float.isNaN(a) && Float.isNaN(b))
+            return true;
+        else if (a == Float.NEGATIVE_INFINITY || a == Float.POSITIVE_INFINITY && b == Float.NEGATIVE_INFINITY || b == Float.POSITIVE_INFINITY){
+            return a == b;
+        }
+
+        return false;
     }
 
     public static void main(String[] args) {
