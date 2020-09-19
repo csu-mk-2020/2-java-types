62d175c5e7267ef71c3e7b395894f30142acbcd6
funin <funin99@bk.ru>
2020-09-19 14:09:58 +0500

    Task12 [Фунин]

--- task12/src/com/example/task12/Task12.java
+++ task12/src/com/example/task12/Task12.java
@@ -1,6 +1,8 @@
 package com.example.task12;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 
 public class Task12 {
 
@@ -10,14 +12,11 @@ public class Task12 {
 
         // Считаем проценты за год
 
-        /*
-        for (int i = 1; i <= 12; i++) {
-            sum += sum * percent;
-        }
-        return sum;
-        */
 
-        return BigDecimal.ZERO;
+        for (int i = 1; i <= 12; i++) {
+            sum = sum.add(sum.multiply(percent));
+        }
+        return sum.setScale(9, BigDecimal.ROUND_HALF_UP);
     }
 
     public static void main(String[] args) {
