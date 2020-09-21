00172dec68157c6faba06c0b9f9a835761403222
Виталий Вересников <Vitosiy21@yandex.ru>
2020-09-21 18:01:47 +0500

    Task05 [Veresnikov]

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -3,19 +3,23 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        while (x > 0) {
+            int tmp = x % 10;
+            x /= 10;
+            if (tmp % 2 == 0) {
+                continue;
+            } else return "FALSE";
+        }
+        return "TRUE";
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         String result = solution(1234);
         System.out.println(result);
-        */
+
     }
 
 }
