7202769869b62dbcc7e190e49bb2d46bf2e83607
myslivko <avm725@gmail.com>
2020-09-22 23:08:02 +0500

    task02[Мысливко]

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -6,7 +6,25 @@ public class Task02 {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
-        return "";
+        long number = Long.parseLong(input);
+        String str = "";
+        int count = 0;
+        if (number <= 127 && number >= -128) {
+            str = "byte";
+            count++;
+        }
+        if (number <= 32767 && number >= -32768 && count == 0) {
+            str = "short";
+            count++;
+        }
+        if (number <= 2147483647 && number >= -2147483648 && count == 0) {
+            str = "int";
+            count++;
+        }
+        if (number <= 9223372036854775807l && number >= -9223372036854775808l && count == 0) {
+            str = "long";
+        }
+        return str;
     }
 
     public static void main(String[] args) {
