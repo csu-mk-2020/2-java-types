04d9388b17abae91ac0bf30df8023ff363fd09a6
Astaroth <avm725@gmail.com>
2020-09-30 01:02:44 +0500

    Task12[Мысливко]

--- task12/src/com/example/task12/Task12.java
+++ task12/src/com/example/task12/Task12.java
@@ -10,14 +10,16 @@ public class Task12 {
 
         // Считаем проценты за год
 
-        /*
+
         for (int i = 1; i <= 12; i++) {
-            sum += sum * percent;
+       /*     sum += sum * percent;
         }
         return sum;
         */
+            sum = sum.add(sum.multiply(percent));
+        }
 
-        return BigDecimal.ZERO;
+        return sum.setScale(9, BigDecimal.ROUND_HALF_UP);
     }
 
     public static void main(String[] args) {
