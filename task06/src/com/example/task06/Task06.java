67683d29b5982753403ead78bcc06aea48db4892
Shinen <Fixmaks99@gmail.com>
2020-09-29 08:31:31 +0500

    2 Task06 Ефимов

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -2,7 +2,7 @@ package com.example.task06;
 
 public class Task06 {
 
-    public static int solution(int x, int y) { return String.valueOf(x+y).length(); }
+    public static int solution(int x, int y) { return String.valueOf(Math.abs(x+y)).length(); }
 
     public static void main(String[] args) {
     }
