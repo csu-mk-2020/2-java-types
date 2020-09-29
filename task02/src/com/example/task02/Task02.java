36f16d0dff766056e9c69a4e23cf49cf351d6c53
Shinen <Fixmaks99@gmail.com>
2020-09-29 08:28:18 +0500

    2 Task02 Ефимов

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -4,10 +4,11 @@ public class Task02 {
 
     public static String solution(String input) {
         long num = Long.parseLong(input);
-        if (num <= Byte.MAX_VALUE) return "byte";
-        if (num <= Short.MAX_VALUE) return "short";
-        if (num <= Integer.MAX_VALUE) return "int";
-        return "long";
+        if (num >= Byte.MIN_VALUE && num <= Byte.MAX_VALUE) return "byte";
+        if (num >= Short.MIN_VALUE && num <= Short.MAX_VALUE) return "short";
+        if (num >= Integer.MIN_VALUE && num <= Integer.MAX_VALUE) return "int";
+        if (num >= Long.MIN_VALUE && num <= Long.MAX_VALUE) return "long";
+        return "Unknown type";
     }
 
     public static void main(String[] args) {
